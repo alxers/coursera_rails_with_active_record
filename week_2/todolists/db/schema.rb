4fcbc3c69fef4c0789b8cf97b2997532915d71e6
Alexey Ershov <alex4rom@gmail.com>
2015-12-19 00:54:01 +0200

    Add todo model, refs

--- week_2/todolists/db/schema.rb
+++ week_2/todolists/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151218121648) do
+ActiveRecord::Schema.define(version: 20151218124534) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -25,12 +25,25 @@ ActiveRecord::Schema.define(version: 20151218121648) do
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
+  create_table "todo_items", force: :cascade do |t|
+    t.date     "due_date"
+    t.string   "title"
+    t.text     "description"
+    t.boolean  "completed"
+    t.integer  "todo_list_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
   create_table "todo_lists", force: :cascade do |t|
     t.string   "list_name"
     t.date     "list_due_date"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
+
+  add_index "todo_lists", ["user_id"], name: "index_todo_lists_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
