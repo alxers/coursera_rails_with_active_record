9515a878f0459d739458a2f2f5c9209d945cad3c
Alexey Ershov <alex4rom@gmail.com>
2015-12-20 00:24:37 +0200

    Finish week 2

--- week_2/todolists/app/models/profile.rb
+++ week_2/todolists/app/models/profile.rb
@@ -1,3 +1,24 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validate :first_or_last_name_presence
+  validates :gender, inclusion: { in: %w( male female ) }
+  validate :correct_name
+
+  def first_or_last_name_presence
+    if first_name.nil? && last_name.nil?
+      errors.add(:first_name, 'at least first or last name should be present')
+      errors.add(:last_name, 'at least first or last name should be present')
+    end
+  end
+
+  def correct_name
+    if gender == 'male' && first_name == 'Sue'
+      errors.add(:first_name, 'is not allowed')
+    end
+  end
+
+  def self.get_all_profiles(min, max)
+    where("birth_year BETWEEN :min and :max", min: min, max: max).order("birth_year ASC")
+  end
 end
--- week_2/todolists/db/seeds.rb
+++ week_2/todolists/db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Profile.destroy_all
+TodoList.destroy_all
+
+User.create! [
+  { username: 'Fiorina', password_digest: '123' },
+  { username: 'Trump', password_digest: '123' },
+  { username: 'Carson', password_digest: '123' },
+  { username: 'Clinton', password_digest: '123' }
+]
+
+profiles = [
+  { first_name: 'Carly', last_name: 'Fiorina', birth_year: 1954, gender: 'female' },
+  { first_name: 'Donald', last_name: 'Trump', birth_year: 1946, gender: 'male' },
+  { first_name: 'Ben', last_name: 'Carson', birth_year: 1951, gender: 'male' },
+  { first_name: 'Hillary', last_name: 'Clinton', birth_year: 1947, gender: 'female' }
+]
+
+todo_lists = [
+  { list_name: 'first', list_due_date: 1.year.since },
+  { list_name: 'second', list_due_date: 1.year.since },
+  { list_name: 'third', list_due_date: 1.year.since },
+  { list_name: 'fourth', list_due_date: 1.year.since }
+]
+
+todo_items = []
+
+5.times do |i|
+  todo_items.push(
+      {
+        due_date: 1.year.since, title: "title #{i}", description: "description #{i}"
+      }
+    )
+end
+
+User.all.each_with_index do |user, index|
+  user.create_profile(profiles[index])
+  user.todo_lists.create(todo_lists[index])
+end
+
+TodoList.all.each_with_index do |list, index|
+  list.todo_items.create(todo_items)
+end
